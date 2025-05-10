480f88d1a826e8c4bdeb2cb39c660eddf9433ba0
chanicpanic <51764816+chanicpanic@users.noreply.github.com>
2025-05-09 22:30:41 -0700

    Create some derived glyphs

--- tools/create_from_reference.py
+++ tools/create_from_reference.py
@@ -18,6 +18,10 @@ transformations = {
         "matrix": psMat.identity(),
         "reverse": False,
     },
+    "R to F": {
+        "matrix": compose(flip_horizontal, psMat.rotate(math.radians(60))),
+        "reverse": False,
+    },
     "R to K": {
         "matrix": psMat.rotate(math.radians(180)),
         "reverse": True,
@@ -27,7 +31,7 @@ transformations = {
         "reverse": True,
     },
     "N to T": {
-        "matrix": psMat.rotate(math.radians(-30)),
+        "matrix": psMat.rotate(math.radians(30)),
         "reverse": False,
     },
 }
@@ -45,12 +49,44 @@ def create_from_reference(data, glyph):
             {
                 "type": "choice",
                 "question": "transformation",
-                "answers": list({"name": t} for t in transformations.keys())
+                "answers": list({"name": t, "default": t == "Identity"} for t in transformations.keys())
             },
         ]
     )
 
-    print(answers)
+    if not answers:
+        return
+
+    reference_glyph = glyph.font[answers["glyph"]]
+
+    entry_anchor = None
+    exit_anchor = None
+    for anchor in reference_glyph.anchorPoints:
+        if anchor[0] == "Join":
+            if anchor[1] == "entry":
+                entry_anchor = (anchor[2], anchor[3])
+            else:
+                exit_anchor = (anchor[2], anchor[3])
+
+    transformation = transformations[answers["transformation"]]
+
+    if transformation["reverse"]:
+        entry_anchor, exit_anchor = exit_anchor, entry_anchor
+
+    if entry_anchor:
+        glyph.addAnchorPoint("Join", "entry", entry_anchor[0], entry_anchor[1])
+    if exit_anchor:
+        glyph.addAnchorPoint("Join", "exit", exit_anchor[0], exit_anchor[1])
+
+    glyph.addReference(reference_glyph.glyphname)
+
+    glyph.transform(transformation["matrix"])
+    glyph.left_side_bearing = 0
+    glyph.right_side_bearing = 0
+
+    (xmin, ymin, xmax, ymax) = glyph.boundingBox()
+    if ymin < 0:
+        glyph.transform(psMat.translate(0, -ymin - glyph.font.strokewidth))
 
 
 fontforge.registerMenuItem(
