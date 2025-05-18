e831628161be51602a7dbdd5fa5b964563708ddb
chanicpanic <51764816+chanicpanic@users.noreply.github.com>
2025-05-17 21:32:12 -0700

    Add glyphs for 'a' after

--- tests/test_substitution.py
+++ tests/test_substitution.py
@@ -22,24 +22,48 @@ def test_multichar_ligatures(font, text, expected_glyphs):
 
 
 @pytest.mark.parametrize("text,expected_glyphs", [
+    ("ak", ["a.ak", "k"]),
+    ("ag", ["a.ak", "g"]),
+    ("ar", ["a.ar", "r"]),
+    ("al", ["a.al", "l"]),
+    ("an", ["a.an", "n"]),
+    ("am", ["a.an", "m"]),
+    ("at", ["a.at", "t"]),
+    ("ad", ["a.at", "d"]),
+    ("ap", ["a.ap", "p"]),
+    ("ab", ["a.ap", "b"]),
+    ("af", ["a.af", "f"]),
+    ("av", ["a.av", "v"]),
     ("ash", ["a.ach", "sh"]),
     ("ach", ["a.ach", "ch"]),
     ("aj", ["a.ach", "j"]),
-    ("af", ["a.af", "f"]),
-    ("av", ["a.av", "v"]),
-    ("ak", ["a.ak", "k"]),
-    ("ag", ["a.ak", "g"]),
-    ("an", ["a.an", "n"]),
-    ("am", ["a.an", "m"]),
-    ("ap", ["a.ap", "p"]),
-    ("ab", ["a.ap", "b"]),
-    ("ar", ["a.ar", "r"]),
-    ("al", ["a.al", "l"]),
     ("ath", ["a.ath", "th"]),
-    ("at", ["a.at", "t"]),
-    ("ad", ["a.at", "d"]),
     ("ang", ["a.ang", "ng"]),
     ("ank", ["a.ang", "nk"]),
 ])
 def test_a_before(font, text, expected_glyphs):
+    assert shape(text, font) == expected_glyphs
+
+
+@pytest.mark.parametrize("text,expected_glyphs", [
+    ("ka", ["k", "a.ka"]),
+    ("ga", ["g", "a.ka"]),
+    ("ra", ["r", "a.ra"]),
+    ("la", ["l", "a.ra"]),
+    ("na", ["n", "a.na"]),
+    ("ma", ["m", "a.na"]),
+    ("ta", ["t", "a.ta"]),
+    ("da", ["d", "a.ta"]),
+    ("pa", ["p", "a.pa"]),
+    ("ba", ["b", "a.ba"]),
+    ("fa", ["f", "a.fa"]),
+    ("va", ["v", "a.fa"]),
+    ("sha", ["sh", "a.cha"]),
+    ("cha", ["ch", "a.cha"]),
+    ("ja", ["j", "a.cha"]),
+    ("tha", ["th", "a.tha"]),
+    ("nga", ["ng", "a.nga"]),
+    ("nka", ["nk", "a.nga"]),
+])
+def test_a_after(font, text, expected_glyphs):
     assert shape(text, font) == expected_glyphs
