e0cf953fc3a5a9500a0985c9704e2d39733c8584
chanicpanic <51764816+chanicpanic@users.noreply.github.com>
2025-06-14 15:16:32 -0700

    Add re* glyphs

--- tests/substitution/test_e.py
+++ tests/substitution/test_e.py
@@ -106,3 +106,57 @@ def test_k_e(font, text, expected_glyphs):
 ])
 def test_g_e(font, text, expected_glyphs):
     assert shape(text, font) == expected_glyphs
+
+
+@pytest.mark.parametrize("text,expected_glyphs", [
+    ("rek",   ["r", "e.ek", "k"]),
+    ("reg",   ["r", "e.ek", "g"]),
+    ("rer",   ["r", "e.rer", "r"]),
+    ("rel",   ["r", "e.rer", "l"]),
+    ("ren",   ["r", "e.ren", "n"]),
+    ("rem",   ["r", "e.ren", "m"]),
+    ("ret",   ["r", "e.ret", "t"]),
+    ("red",   ["r", "e.ret", "d"]),
+    ("rep",   ["r", "e.rep", "p"]),
+    ("reb",   ["r", "e.rep", "b"]),
+    ("ref",   ["r", "e.ref", "f.cut"]),
+    ("rev",   ["r", "e.ref", "v.cut"]),
+    ("resh",  ["r", "e.rech", "sh"]),
+    ("rech",  ["r", "e.rech", "ch"]),
+    ("rej",   ["r", "e.rech", "j"]),
+    ("res",   ["r", "e.rep", "s.left"]),
+    ("res)",  ["r", "e.ref", "s.right.cut"]),
+    ("reng",  ["r", "e.reng", "ng"]),
+    ("renk",  ["r", "e.reng", "nk"]),
+    ("reth(", ["r", "e.retn", "th.over.skew30"]),
+    ("reth",  ["r", "e.rent", "th.under"]),
+])
+def test_r_e(font, text, expected_glyphs):
+    assert shape(text, font) == expected_glyphs
+
+
+@pytest.mark.parametrize("text,expected_glyphs", [
+    ("lek",   ["l", "e.ek", "k"]),
+    ("leg",   ["l", "e.ek", "g"]),
+    ("ler",   ["l", "e.rer", "r"]),
+    ("lel",   ["l", "e.rer", "l"]),
+    ("len",   ["l", "e.ren", "n"]),
+    ("lem",   ["l", "e.ren", "m"]),
+    ("let",   ["l", "e.ret", "t"]),
+    ("led",   ["l", "e.ret", "d"]),
+    ("lep",   ["l", "e.rep", "p"]),
+    ("leb",   ["l", "e.rep", "b"]),
+    ("lef",   ["l", "e.ref", "f.cut"]),
+    ("lev",   ["l", "e.ref", "v.cut"]),
+    ("lesh",  ["l", "e.rech", "sh"]),
+    ("lech",  ["l", "e.rech", "ch"]),
+    ("lej",   ["l", "e.rech", "j"]),
+    ("les",   ["l", "e.rep", "s.left"]),
+    ("les)",  ["l", "e.ref", "s.right.cut"]),
+    ("leng",  ["l", "e.reng", "ng"]),
+    ("lenk",  ["l", "e.reng", "nk"]),
+    ("leth(", ["l", "e.retn", "th.over.skew30"]),
+    ("leth",  ["l", "e.rent", "th.under"]),
+])
+def test_l_e(font, text, expected_glyphs):
+    assert shape(text, font) == expected_glyphs
