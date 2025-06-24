6931e162426c5632dd362775d03261f35cdf326c
chanicpanic <51764816+chanicpanic@users.noreply.github.com>
2025-06-23 20:30:17 -0700

    Add ko* glyphs

--- tests/substitution/test_o.py
+++ tests/substitution/test_o.py
@@ -52,5 +52,57 @@ def test_o_before(font, text, expected_glyphs):
     ("ngo",  ["ng", "o"]),
     ("nko",  ["nk", "o"]),
 ])
-def test_e_after(font, text, expected_glyphs):
+def test_o_after(font, text, expected_glyphs):
+    assert shape(text, font) == expected_glyphs
+
+
+@pytest.mark.parametrize("text,expected_glyphs", [
+    ("kok",  ["k", "o.kok", "k"]),
+    ("kog",  ["k", "o.kok", "g"]),
+    ("kor",  ["k", "o.or", "r.cut"]),
+    ("kol",  ["k", "o.ol", "l.cut"]),
+    ("kon",  ["k", "o.on", "n"]),
+    ("kom",  ["k", "o.on", "m"]),
+    ("kot",  ["k", "o.kot", "t"]),
+    ("kod",  ["k", "o.kot", "d"]),
+    ("kop",  ["k", "o.kop", "p"]),
+    ("kob",  ["k", "o.kop", "b"]),
+    ("kof",  ["k", "o.ko", "f"]),
+    ("kov",  ["k", "o.ko", "v"]),
+    ("kosh", ["k", "o.ko", "sh"]),
+    ("koch", ["k", "o.ko", "ch"]),
+    ("koj",  ["k", "o.ko", "j"]),
+    ("kos)", ["k", "o.ko", "s.right"]),
+    ("kos(", ["k", "o.kop", "s.left"]),
+    ("kong", ["k", "o.ong", "ng"]),
+    ("konk", ["k", "o.ong", "nk"]),
+    # ("koth", ["k", "o", "th.under"]),
+])
+def test_g_o(font, text, expected_glyphs):
+    assert shape(text, font) == expected_glyphs
+
+
+@pytest.mark.parametrize("text,expected_glyphs", [
+    ("gok",  ["g", "o.kok", "k"]),
+    ("gog",  ["g", "o.kok", "g"]),
+    ("gor",  ["g", "o.or", "r.cut"]),
+    ("gol",  ["g", "o.ol", "l.cut"]),
+    ("gon",  ["g", "o.on", "n"]),
+    ("gom",  ["g", "o.on", "m"]),
+    ("got",  ["g", "o.kot", "t"]),
+    ("god",  ["g", "o.kot", "d"]),
+    ("gop",  ["g", "o.kop", "p"]),
+    ("gob",  ["g", "o.kop", "b"]),
+    ("gof",  ["g", "o.ko", "f"]),
+    ("gov",  ["g", "o.ko", "v"]),
+    ("gosh", ["g", "o.ko", "sh"]),
+    ("goch", ["g", "o.ko", "ch"]),
+    ("goj",  ["g", "o.ko", "j"]),
+    ("gos)", ["g", "o.ko", "s.right"]),
+    ("gos(", ["g", "o.kop", "s.left"]),
+    ("gong", ["g", "o.ong", "ng"]),
+    ("gonk", ["g", "o.ong", "nk"]),
+    # ("goth", ["g", "o", "th.under"]),
+])
+def test_g_o(font, text, expected_glyphs):
     assert shape(text, font) == expected_glyphs
