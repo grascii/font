84e421c873a20ec2ba10702a5bdaab79f5e511a3
chanicpanic <51764816+chanicpanic@users.noreply.github.com>
2025-08-23 14:56:14 -0700

    Add ri* glyphs

--- tests/substitution/test_i.py
+++ tests/substitution/test_i.py
@@ -80,3 +80,52 @@ def test_k_i(font, text, expected_glyphs):
 def test_g_i(font, text, expected_glyphs):
     assert shape(text, font) == expected_glyphs
 
+
+@pytest.mark.parametrize("text,expected_glyphs", [
+    ("rik",   ["r", "i.rik", "k"]),
+    ("rig",   ["r", "i.rik", "g"]),
+    ("rir",   ["r", "i.ri", "r"]),
+    ("ril",   ["r", "i.ri", "l"]),
+    ("rin",   ["r", "i.ri", "n"]),
+    ("rim",   ["r", "i.ri", "m"]),
+    ("rit",   ["r", "i.ri", "t"]),
+    ("rid",   ["r", "i.ri", "d"]),
+    ("rip",   ["r", "i.ri", "p"]),
+    ("rib",   ["r", "i.ri", "b"]),
+    ("rif",   ["r", "i.ri", "f.cut"]),
+    ("riv",   ["r", "i.ri", "v.cut"]),
+    ("rish",  ["r", "i.ri", "sh"]),
+    ("rich",  ["r", "i.ri", "ch"]),
+    ("rij",   ["r", "i.ri", "j"]),
+    ("ris)",  ["r", "i.ri", "s.right.cut"]),
+    ("ris",   ["r", "i.ri", "s.left"]),
+    ("rith",  ["r", "i.ri", "th.under"]),
+])
+def test_r_i(font, text, expected_glyphs):
+    assert shape(text, font) == expected_glyphs
+
+
+@pytest.mark.parametrize("text,expected_glyphs", [
+    ("lik",   ["l", "i.rik", "k"]),
+    ("lig",   ["l", "i.rik", "g"]),
+    ("lir",   ["l", "i.ri", "r"]),
+    ("lil",   ["l", "i.ri", "l"]),
+    ("lin",   ["l", "i.ri", "n"]),
+    ("lim",   ["l", "i.ri", "m"]),
+    ("lit",   ["l", "i.ri", "t"]),
+    ("lid",   ["l", "i.ri", "d"]),
+    ("lip",   ["l", "i.ri", "p"]),
+    ("lib",   ["l", "i.ri", "b"]),
+    ("lif",   ["l", "i.ri", "f.cut"]),
+    ("liv",   ["l", "i.ri", "v.cut"]),
+    ("lish",  ["l", "i.ri", "sh"]),
+    ("lich",  ["l", "i.ri", "ch"]),
+    ("lij",   ["l", "i.ri", "j"]),
+    ("lis)",  ["l", "i.ri", "s.right.cut"]),
+    ("lis",   ["l", "i.ri", "s.left"]),
+    ("lith",  ["l", "i.ri", "th.under"]),
+])
+def test_l_i(font, text, expected_glyphs):
+    assert shape(text, font) == expected_glyphs
+
+
