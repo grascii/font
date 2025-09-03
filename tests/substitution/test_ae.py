973660be3e59cc00ad47aea45d781570e2e92fcd
chanicpanic <51764816+chanicpanic@users.noreply.github.com>
2025-09-02 22:06:01 -0700

    Add ra&e* glyphs

--- tests/substitution/test_ae.py
+++ tests/substitution/test_ae.py
@@ -51,3 +51,31 @@ def test_k_ae(font, text, expected_glyphs):
 def test_g_ae(font, text, expected_glyphs):
     assert shape(text, font) == expected_glyphs
 
+
+@pytest.mark.parametrize("text,expected_glyphs", [
+    ("ra&er",  ["r", "ae.raer", "r"]),
+    ("ra&el",  ["r", "ae.raer", "l"]),
+    ("ra&en",  ["r", "a.ran", "e.ren", "n"]),
+    ("ra&em",  ["r", "a.ran", "e.ren", "m"]),
+    ("ra&et",  ["r", "ae.raet", "t"]),
+    ("ra&ed",  ["r", "ae.raet", "d"]),
+    ("ra&eng", ["r", "a.rang", "e.reng", "ng"]),
+    ("ra&enk", ["r", "a.rang", "e.reng", "nk"]),
+])
+def test_r_ae(font, text, expected_glyphs):
+    assert shape(text, font) == expected_glyphs
+
+
+@pytest.mark.parametrize("text,expected_glyphs", [
+    ("la&er",  ["l", "ae.raer", "r"]),
+    ("la&el",  ["l", "ae.raer", "l"]),
+    ("la&en",  ["l", "a.ran", "e.ren", "n"]),
+    ("la&em",  ["l", "a.ran", "e.ren", "m"]),
+    ("la&et",  ["l", "ae.raet", "t"]),
+    ("la&ed",  ["l", "ae.raet", "d"]),
+    ("la&eng", ["l", "a.rang", "e.reng", "ng"]),
+    ("la&enk", ["l", "a.rang", "e.reng", "nk"]),
+])
+def test_l_ae(font, text, expected_glyphs):
+    assert shape(text, font) == expected_glyphs
+
