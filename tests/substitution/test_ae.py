611a2171bc57884c284596ad701a4b5888185c81
chanicpanic <51764816+chanicpanic@users.noreply.github.com>
2025-09-02 20:33:24 -0700

    Add ka&e* glyphs

--- tests/substitution/test_ae.py
+++ tests/substitution/test_ae.py
@@ -31,3 +31,23 @@ def test_ae_before(font, text, expected_glyphs):
 def test_ae_after(font, text, expected_glyphs):
     assert shape(text, font) == expected_glyphs
 
+
+@pytest.mark.parametrize("text,expected_glyphs", [
+    ("ka&en", ["k", "ae.kaen", "n"]),
+    ("ka&em", ["k", "ae.kaen", "m"]),
+    ("ka&et", ["k", "ae.kaet", "t"]),
+    ("ka&ed", ["k", "ae.kaet", "d"]),
+])
+def test_k_ae(font, text, expected_glyphs):
+    assert shape(text, font) == expected_glyphs
+
+
+@pytest.mark.parametrize("text,expected_glyphs", [
+    ("ga&en", ["g", "ae.kaen", "n"]),
+    ("ga&em", ["g", "ae.kaen", "m"]),
+    ("ga&et", ["g", "ae.kaet", "t"]),
+    ("ga&ed", ["g", "ae.kaet", "d"]),
+])
+def test_g_ae(font, text, expected_glyphs):
+    assert shape(text, font) == expected_glyphs
+
